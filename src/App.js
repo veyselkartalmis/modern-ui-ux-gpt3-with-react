2a5fe597d76cc06ffecfca10087e7c60651ad855
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-05-12 16:59:28 +0300

    navbar added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
 import { CTA, Brand, Navbar } from './components';
-
 import './App.css';
 
 const App = () => (
